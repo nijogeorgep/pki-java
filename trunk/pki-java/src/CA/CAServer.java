3138d85c7d03916386b7fce14d57907fac545d09
pierre.e.junk@gmail.com <pierre.e.junk@gmail.com@a9008878-dd8b-735c-9322-b12ffec8bbc9>
2012-04-30 13:39:26 +0000

    CA operationnel

--- trunk/pki-java/src/CA/CAServer.java
+++ trunk/pki-java/src/CA/CAServer.java
@@ -1,12 +1,28 @@
 package CA;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.math.BigInteger;
 import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-
 import java.nio.ByteBuffer;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.Security;
+import java.security.SignatureException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
@@ -14,67 +30,83 @@ import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import org.bouncycastle.asn1.pkcs.Attribute;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.X509Name;
+import org.bouncycastle.cert.ocsp.OCSPReq;
+import org.bouncycastle.cert.ocsp.OCSPResp;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 
+import com.sun.jndi.ldap.LdapClient;
+
+import CryptoAPI.CSRManager;
+import CryptoAPI.OCSPManager;
+import Ldap.LDAP;
+import Ldap.ldaputils;
+import Utils.Config;
 
 public class CAServer {
 
-	ServerSocketChannel s;			//server socket
-	ByteBuffer masterBuffer;		//buffer used to store temporarily bytes read in sockets
-	Selector sel;
-	SelectionKey keyserver;			//SelectionKey of the server
-	
-    public static void main(String[] args) {
+  ServerSocketChannel s;      //server socket
+  ByteBuffer masterBuffer;    //buffer used to store temporarily bytes read in sockets
+  Selector sel;
+  SelectionKey keyserver;     //SelectionKey of the server
+
+  KeyStore ks;
+  
+    public static void main(String[] args) throws UnrecoverableKeyException, InvalidKeyException, KeyStoreException, NoSuchAlgorithmException, NoSuchProviderException, SignatureException, OperatorCreationException, CertificateException {
+      Security.addProvider(new BouncyCastleProvider());
         try {
-        	
-        	/*######### TODO #########
-        	 * Avant d'écouter les requetes on doit :
-        	 * Ouvrir le KeyStore qui contient globalement que le certificat privé de la CA (ou alors c'est le role du constructeur ?)
-        	 * 
-        	 *#######################*/
-        	
             CAServer s = new CAServer();
             s.run();
         } catch (IOException e) {
             System.out.println("Bug !\n" +e);
+            e.printStackTrace();
         } catch (InterruptedException e) {
             System.err.println("Interrupted !");
         } 
     }
     
-	public CAServer() throws IOException, InterruptedException {
-		this.s = ServerSocketChannel.open();
-		this.s.socket().bind(new InetSocketAddress(5555));		//arbitrarily set to 5555
-		this.s.configureBlocking(false);
-		this.masterBuffer = ByteBuffer.allocate(4096);
-		
-		this.sel = Selector.open();
-		this.keyserver= s.register(this.sel, SelectionKey.OP_ACCEPT);	//register the server selectionkey in accept
-	}
-	
-	//main method in wich the main thread will be jailed.
-	public void run() throws IOException, InterruptedException
-	{
-		for(;;) {
-			this.sel.select(); // wait for an event
-			
-			Set keys = this.sel.selectedKeys();
-			Iterator i = keys.iterator();
-			while( i.hasNext())
-			{
-				SelectionKey sk = (SelectionKey) i.next();
-				i.remove();
-				
-				if(sk.isValid()) {  //Get in only if the key is valid, which is not always the case
-				
-					if(sk == this.keyserver && sk.isAcceptable()) {  // sk == this.keyserver is optionnal because there's just the server registered in accept
-						SocketChannel client = ((ServerSocketChannel)sk.channel()).accept();
-						client.configureBlocking(false);										//configure client in non-blocking
-						client.register(this.sel, SelectionKey.OP_READ); //register the client in READ with the given attachment (no need to do key.attach)
-						// ## ! It's just above to put an attachment to the key if needed ! (client.register(this.sel, SelectionKey.OP_READ,ATTACHMENT);
-					}
-					
-					if ( sk.isReadable()) {
-						SocketChannel client =  (SocketChannel) sk.channel(); //gather the socket that triggered the read event
+  public CAServer() throws IOException, InterruptedException {
+    this.s = ServerSocketChannel.open();
+    this.s.socket().bind(new InetSocketAddress(5555));    //arbitrarily set to 5555
+    this.s.configureBlocking(false);
+    this.masterBuffer = ByteBuffer.allocate(4096);
+    this.sel = Selector.open();
+    this.keyserver= s.register(this.sel, SelectionKey.OP_ACCEPT); //register the server selectionkey in accept
+
+    try {
+      this.ks = KeyStore.getInstance(KeyStore.getDefaultType()); //Je load tout les certificats en mémoire pour les avoir directement sous la main
+      this.ks.load(new FileInputStream("src/Playground/test_keystore.ks"), "passwd".toCharArray());
+    } catch (Exception e) { e.printStackTrace();}
+    
+  }
+  
+  //main method in wich the main thread will be jailed.
+  public void run() throws IOException, InterruptedException, UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException, InvalidKeyException, NoSuchProviderException, SignatureException, OperatorCreationException, CertificateException
+  {
+    for(;;) {
+      this.sel.select(); // wait for an event
+      
+      Set keys = this.sel.selectedKeys();
+      Iterator i = keys.iterator();
+      while( i.hasNext())
+      {
+        SelectionKey sk = (SelectionKey) i.next();
+        i.remove();
+        
+        if(sk.isValid()) {  //Get in only if the key is valid, which is not always the case
+        
+          if(sk == this.keyserver && sk.isAcceptable()) {
+            SocketChannel client = ((ServerSocketChannel)sk.channel()).accept();
+            client.configureBlocking(false);                    //configure client in non-blocking
+            client.register(this.sel, SelectionKey.OP_READ); //register the client in READ with the given attachment (no need to do key.attach)
+        }
+          
+          if ( sk.isReadable()) {
+            SocketChannel client =  (SocketChannel) sk.channel(); //gather the socket that triggered the read event
 
                         this.masterBuffer.clear(); // clear the main buffer and read the client message.
                         try {
@@ -84,59 +116,63 @@ public class CAServer {
                                 continue; // avoid an CancelledKeyexception (because if we close client the key (sk) is not valid anymore and if(sk.isWritable()) will raise exception)
                             }
                             else {
-                                /* ############# TODO #############
-                                 * Par convention on décide que le CA n'accepte que les connections du RA donc uns fois par connection on doit :
-                                 * - Receptionner un message signé et le vérifier avec le certificat du RA (qui doit se trouver dans notre keystore).
-                                 * Attention: tout ca ne doit se faire qu'un fois par connection (il est donc peut être bon de passer la selectionkey en writable
-                                 * En cas d'échec on ferme la socket
-                                 * Si tout est ok on receptionne la requete du RA qui peut être : signature d'une CSR, signature d'un révocation (et peut être d'autres trucs je sais pas)
-                                 * L'ideal serait que le CA receptionne le truc a signer et détecte dynamiquement si c'est une CSR ou une révocation (ou autre)
-                                 *###############################*/
-                            	
-                            	// ### L'ideal serait que une fois qu'on à récupéré la requete du RA on passe la key en writable et qu'on réponde les données a ce moment là.
-                            	
-                                // play with attachment etc..
-                                //sk.interestOps(SelectionKey.OP_READ|SelectionKey.OP_WRITE);
-                            }						
+                              //if(client.socket().getInetAddress().equals(Utils.Config.get("IP_RA", "")))
+                              if(true)
+                              {
+                                //r�cup�ration du csr
+                                PKCS10CertificationRequest csr = new PKCS10CertificationRequest( readBuff(byteread));
+                                PrivateKey pk = (PrivateKey) ks.getKey("CA_IntermediairePeople_Private", Config.get("PASSWORD_CA_INTP", "default_val").toCharArray());
+                                //cr�ation d'un certificat sign�
+                                 BigInteger bigInt = new BigInteger(ldaputils.getUIDFromSubject(csr.getSubject().toString()));
+                                 
+                                 X509Certificate c = CSRManager.retrieveCertificateFromCSR(csr,pk , (X509Certificate)ks.getCertificate("CA_IntermediairePeople_Certificate"), bigInt);
+                                 sk.attach(c.getEncoded());
+                                 sk.interestOps(SelectionKey.OP_READ|SelectionKey.OP_WRITE);
+                              }
+                              else
+                              {
+                                System.out.println("vous n'�tes pas autoris� � vous connecter au CA");
+                                sk.interestOps(SelectionKey.OP_READ|SelectionKey.OP_WRITE);
+                              }
+                            }           
                         } 
                         catch (IOException e) {
                             System.err.println("Client closed unexpectedly!");
                             client.close();
                             continue;
                         }
-					}
-					
-					if (sk.isWritable()) {
-
-						
-						
-						// do whatever you want, play with attachment etc..
-						//sk.interestOps(SelectionKey.OP_READ); //set back the client in read mode
-						
-					}
-				}
-				
-			}
-		}
-	}
-	
-	private byte[] readBuff(int val) { //method to transform buffer into byte[]
+          }
+          
+          if (sk.isWritable()) {
+            byte[] attachment = (byte[]) sk.attachment(); //On récupère l'attachment
+            SocketChannel client =  (SocketChannel) sk.channel(); 
+            client.write(ByteBuffer.wrap(attachment)); //On écrit ce que l'on a récupéré
+            
+            sk.interestOps(SelectionKey.OP_READ); //On repasse la clé en read
+          }
+        }
+        
+      }
+    }
+  }
+  
+  private byte[] readBuff(int val) { //method to transform buffer into byte[]
         this.masterBuffer.flip();
-		byte myarray[] = new byte[val];
-		
-		for(int i=0; this.masterBuffer.hasRemaining(); i++) {
-			myarray [i]= this.masterBuffer.get();
-		}
-		return myarray;
-	}
+    byte myarray[] = new byte[val];
+    
+    for(int i=0; this.masterBuffer.hasRemaining(); i++) {
+      myarray [i]= this.masterBuffer.get();
+    }
+    return myarray;
+  }
       
     private void writeSocket(SelectionKey k, ByteBuffer b) {
-    	SocketChannel client =  (SocketChannel) k.channel(); // gather the client socket
-    	try {
-			client.write(b);								//write the message to the client
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+      SocketChannel client =  (SocketChannel) k.channel(); // gather the client socket
+      try {
+      client.write(b);                //write the message to the client
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
     }
     
 }
