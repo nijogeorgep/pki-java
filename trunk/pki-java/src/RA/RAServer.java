ec3e1c7d3aa94bab3322cea2df5fa7c84e72a9ed
mail.robin.david@gmail.com <mail.robin.david@gmail.com@a9008878-dd8b-735c-9322-b12ffec8bbc9>
2012-04-30 13:29:12 +0000

    Debug RAserver

--- trunk/pki-java/src/RA/RAServer.java
+++ trunk/pki-java/src/RA/RAServer.java
@@ -1,14 +1,15 @@
 package RA;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.math.BigInteger;
 import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.ByteBuffer;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
@@ -18,6 +19,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 
+import Utils.Config;
 
 public class RAServer {
 
@@ -25,8 +27,11 @@ public class RAServer {
 	ByteBuffer masterBuffer;		//buffer used to store temporarily bytes read in sockets
 	Selector sel;
 	SelectionKey keyserver;			//SelectionKey of the server
-	
-	
+	//------ Ajout --------
+	X509Certificate caSignerCert;
+	PrivateKey caSignerKey;
+	KeyStore ks;
+	//----------------------
     public static void main(String[] args) {
         try {
             RAServer s = new RAServer();
@@ -40,12 +45,20 @@ public class RAServer {
     
 	public RAServer() throws IOException, InterruptedException {
 		this.s = ServerSocketChannel.open();
-		this.s.socket().bind(new InetSocketAddress(7000));		//arbitrarily set to 5555
+		this.s.socket().bind(new InetSocketAddress(5555));		//arbitrarily set to 5555
 		this.s.configureBlocking(false);
 		this.masterBuffer = ByteBuffer.allocate(4096);
 		
 		this.sel = Selector.open();
 		this.keyserver= s.register(this.sel, SelectionKey.OP_ACCEPT);	//register the server selectionkey in accept
+		//----- Added -----
+		try {
+			this.ks = KeyStore.getInstance(KeyStore.getDefaultType()); //Je load tout les certificats en mémoire pour les avoir directement sous la main
+			this.ks.load(new FileInputStream("src/Playground/test_keystore.ks"), "passwd".toCharArray());
+			this.caSignerCert = (X509Certificate) ks.getCertificate("CA_SigningOnly_Certificate");
+			this.caSignerKey = (PrivateKey) ks.getKey("CA_SigningOnly_Private", Config.get("PASSWORD_CA_SIG","").toCharArray());
+		} catch (Exception e) { e.printStackTrace();}
+		//--------------------
 		/* ############# TODO ###############
 		 * Voici les autres choses que doit faire le constructeur
 		 * - Lancer un thread autonome qui periodiquement :
@@ -74,102 +87,66 @@ public class RAServer {
 					if(sk == this.keyserver && sk.isAcceptable()) {  // sk == this.keyserver is optionnal because there's just the server registered in accept
 						SocketChannel client = ((ServerSocketChannel)sk.channel()).accept();
 						client.configureBlocking(false);										//configure client in non-blocking
-						BlockingQueue<ByteBuffer> bbq = new LinkedBlockingQueue<ByteBuffer>() ;
-						client.register(this.sel, SelectionKey.OP_READ,bbq); //register the client in READ with the given attachment (no need to do key.attach)
-						// ## ! It's just above to put an attachment to the key if needed ! (client.register(this.sel, SelectionKey.OP_READ,ATTACHMENT);
+						client.register(this.sel, SelectionKey.OP_READ); //register the client in READ with the given attachment (no need to do key.attach)
 					}
 					
 					if ( sk.isReadable()) {
 						SocketChannel client =  (SocketChannel) sk.channel(); //gather the socket that triggered the read event
-						
-            this.masterBuffer.clear(); // clear the main buffer and read the
-                                       // client message.
-            try
-            {
-              int byteread = client.read(this.masterBuffer);
-              if (byteread == -1)
-              {
-                client.close();
-                continue; // avoid an CancelledKeyexception (because if we close client the key (sk) is not valid anymore and
-                          // if(sk.isWritable()) will raise exception)
-              }
-              else
-              {
-                
-                ((BlockingQueue<ByteBuffer>) sk.attachment()).add(ByteBuffer.wrap(this.readBuff(byteread))); //add the bytebuffer in the client BlockingQueue
-                sk.interestOps(SelectionKey.OP_READ|SelectionKey.OP_WRITE);
 
-                //  byte [] lu =  readBuff(byteread);
-               // System.out.println("coucou : " + new BigInteger(lu)) ;
-               // System.out.println("coucou : " + new String(lu)) ;
-                //PKCS10CertificationRequest certif = new PKCS10CertificationRequest(readBuff(byteread));
-              
-                //certif.toString();
-
-                /*
-                 * ############# TODO ############# Par convention on décide
-                 * que le CA n'accepte que les connections du RA donc uns fois
-                 * par connection on doit : - Receptionner un message signé et
-                 * le vérifier avec le certificat du RA (qui doit se trouver
-                 * dans notre keystore). Attention: tout ca ne doit se faire
-                 * qu'un fois par connection (il est donc peut être bon de
-                 * passer la selectionkey en writable En cas d'échec on ferme
-                 * la socket Si tout est ok on receptionne la requete du RA qui
-                 * peut être : signature d'une CSR, signature d'un révocation
-                 * (et peut être d'autres trucs je sais pas) L'ideal serait que
-                 * le CA receptionne le truc a signer et détecte dynamiquement
-                 * si c'est une CSR ou une révocation (ou autre)
-                 * ###############################
-                 */
-
-                // play with attachment etc..
-                // sk.interestOps(SelectionKey.OP_READ|SelectionKey.OP_WRITE);
-              }
-            }
-            catch (IOException e)
-            {
-              System.err.println("Client closed unexpectedly!");
-              client.close();
-              continue;
-            }
-          }
+                        this.masterBuffer.clear(); // clear the main buffer and read the client message.
+                        try {
+                            int byteread = client.read(this.masterBuffer);
+                            if (byteread == -1) {
+                                client.close();
+                                continue; // avoid an CancelledKeyexception (because if we close client the key (sk) is not valid anymore and if(sk.isWritable()) will raise exception)
+                            }
+                            else {
+                        		byte[] received = readBuff(byteread);
+                            	if(sk.attachment() == null) {
+                            		try {
+                            			PKCS10CertificationRequest request = new PKCS10CertificationRequest(received);
+                            			CSRHandlerThread cli = new CSRHandlerThread(request);
+                            			cli.start();
+                            			sk.attach(cli);
+                            			System.out.println(request.getEncoded());
+                            		}
+                            		catch(Exception e) {//c'est une demande de revocation
+                            			//On fait la même chose qu'au dessus mais avec la classe qui gère la revocation
+                            		}
+                            	}
+                            	else {
+                            		CommunicationHandler ch = (CommunicationHandler) sk.attachment();
+                            		ch.setRead(received);
+                            		// On lit le tableau de byte en on l'envoie pel mel dans l'objet qu'on a caster en l'un ou l'autre.
+                            		//On passe la keys en write
+                            	}
+                            	sk.interestOps(SelectionKey.OP_WRITE);
+                            	
+                            }						
+                        } 
+                        catch (IOException e) {
+                            System.err.println("Client closed unexpectedly!");
+                            client.close();
+                            continue;
+                        }
+					}
 					
-          if (sk.isWritable())
-          {
-            System.out.println("en mode writable");
-            BlockingQueue<ByteBuffer> bq = ((BlockingQueue<ByteBuffer>) sk.attachment());
-            while (!(bq.isEmpty()))
-            {
-              System.out.println("ca boucle pour moi");
-              byte[] lu = null ;
-              while(!bq.isEmpty())
-              {
-                lu = ((ByteBuffer) bq.take()).array();
-                
-                if(new String(lu) != null)
-                {
-                  System.out.println("t'es un zero");
-                  System.out.println(new String(lu));
-                }
-                // traitement pwd
-                if(new BigInteger(lu)!=null)
-                {
-                  System.out.println("Zero comme Heros");
-                  System.out.println(new BigInteger(lu));
-                }
-                // traitement csr ou bigint
-              }
-             
-              // System.out.println("coucou : " + new BigInteger(lu)) ;
-              // System.out.println("coucou : " + new String(lu)) ;
-            }
-
-            sk.interestOps(SelectionKey.OP_READ); 
-            // do whatever you want, play with attachment etc..
-            // sk.interestOps(SelectionKey.OP_READ); //set back the client in
-            // read mode
-
-          }
+					if (sk.isWritable()) {
+						
+						SocketChannel client =  (SocketChannel) sk.channel(); 
+						//CSRHandlerThread cli = (CSRHandlerThread) sk.attachment(); //On essayera systematiquement de caster en CaClientThread si sa échou sa veut dire que c'est un Revocation..
+						CommunicationHandler ch = (CommunicationHandler)sk.attachment();
+						if (ch.getBytesToWrite() == null) {
+							//System.out.println("nothing to do !");
+						}
+						else {
+							System.out.println("Will write:"+ ch.getBytesToWrite());
+							client.write(ByteBuffer.wrap(ch.getBytesToWrite())); //On écrit ce qu'il y a dans et l'on ne ce soucis pas des données
+							ch.resetBytesToWrite();
+							sk.interestOps(SelectionKey.OP_READ);
+						}
+						
+					}
 				}
 				
 			}
