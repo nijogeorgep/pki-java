7f6568f66477da25c01a988f42b7e875d9c0973d
pierre.e.junk@gmail.com <pierre.e.junk@gmail.com@a9008878-dd8b-735c-9322-b12ffec8bbc9>
2012-05-01 14:38:17 +0000

    NDSK with generateSessionKey

--- trunk/pki-java/src/CryptoAPI/NeedhamSchroederPublicKey.java
+++ trunk/pki-java/src/CryptoAPI/NeedhamSchroederPublicKey.java
@@ -2,16 +2,24 @@ package CryptoAPI;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.net.Socket;
 import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.Security;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.OperatorCreationException;
 
 import Ldap.ldaputils;
 import Utils.ClientManager;
@@ -110,6 +118,7 @@ public class NeedhamSchroederPublicKey
          return null ;
        }
     }
+    
     catch (Exception e)
     {
       e.printStackTrace();
@@ -117,9 +126,49 @@ public class NeedhamSchroederPublicKey
     }
   }
   
-  public static void main(String[] args)
+  
+  public static byte[] generateSessionKey(BigInteger a, BigInteger b)
   {
+    byte[] aArray = a.toByteArray();
+    byte[] bArray = b.toByteArray();
+    
+    byte[] tmp = new byte[aArray.length+bArray.length] ;
+    for(int i = 0 ; i < aArray.length; i++)
+    {
+      tmp[i]=aArray[i];
+    }
+    for(int j = aArray.length ; j < tmp.length ; j++ )
+    {
+      tmp[j]= bArray[j-aArray.length];
+    }
+    return MessageDigestUtils.digest(tmp);
+  }
+  
+  public static void main(String[]args) throws NoSuchAlgorithmException, OperatorCreationException, CertificateException, IOException
+  {
+    
+    BigInteger bigint = NeedhamSchroeder.generateNonce();
+    BigInteger bigint2 = NeedhamSchroeder.generateNonce();
+    System.out.println(bigint);
+    System.out.println(bigint2);
 
+    Security.addProvider(new BouncyCastleProvider());
+    KeyPair   kp = KeyPairGenerator.getInstance("RSA").generateKeyPair();
+    KeyPair   kp2 = KeyPairGenerator.getInstance("RSA").generateKeyPair();
+    X509Certificate b =  CertificateManager.createSelfSignedCertificate("Coucou toto", kp);
+    X509Certificate a =  CertificateManager.createSelfSignedCertificate("Calu tata", kp2);
+    
+    byte[] code = NeedhamSchroeder.firstStep(b, bigint);
+    
+    System.out.println("fin step one " + new BigInteger(code));
+    
+    byte[] decode = NeedhamSchroeder.secondStep(bigint2, code, b, a, kp.getPrivate());
+    
+    System.out.println("fin step two " + new BigInteger(decode));
+    
+    byte[] last = NeedhamSchroeder.thirdStep(decode, b, a, bigint, kp2.getPrivate());
+    
+   
   }
 
 }
