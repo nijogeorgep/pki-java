2f8dc520e2b4fea2c377e1cca6b67d8437028e7a
mail.robin.david <mail.robin.david@gmail.com>
2012-05-04 18:19:46 +0000

    Amelioration de la concurrence dans ConnectionChat.

--- pki-java/src/Clients/ConnectionChat.java
+++ pki-java/src/Clients/ConnectionChat.java
@@ -5,16 +5,20 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.math.BigInteger;
+import java.net.BindException;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
+import CryptoAPI.NeedhamSchroeder;
 import CryptoAPI.SymmetricKeyManager;
 
 public class ConnectionChat extends Connection{
 
 	byte[] key;
-	boolean ended = false;
-	boolean diedOk = true;
+	//boolean ended = false;
+	//boolean diedOk = true;
 	
 	public ConnectionChat(String ip, Integer port, Socket s, byte[] key) {
 		super(ip,port);
@@ -33,7 +37,7 @@ public class ConnectionChat extends Connection{
 	
 	
 	public void run() {
-		InputThread th = new InputThread(this.out, this.ended, this.diedOk);
+		InputThread th = new InputThread(this.out);
 		th.start();
 		
 		byte[] received;
@@ -44,15 +48,15 @@ public class ConnectionChat extends Connection{
 					break;
 				received = this.read();
 				inputmessage = new String(SymmetricKeyManager.decipher(key, received));
-				if (inputmessage.equals("bye"))
+				if (inputmessage.equals("bye")) {
+					this.out.write(SymmetricKeyManager.cipher(key, "bye".getBytes()));
+					if (!(th.ended))
+						System.out.println("Client said bye\n Please Press a Key..");
 					break;
-				else
+				}
 					System.out.println(inputmessage);
 			}
-			if(!(th.diedok))
-				this.finishedOK = false;
-			else
-				this.finishedOK = true;
+			this.finishedOK = true;
 			th.dieNow = true;
 		}
 		catch(IOException e) {
@@ -80,13 +84,11 @@ public class ConnectionChat extends Connection{
 	private class InputThread extends Thread implements Runnable {
 		String outputmessage;
 		OutputStream out;
-		boolean ended;
-		boolean diedok;
+		boolean ended = false;
+		//boolean diedok = true;
 		boolean dieNow = false;
-		public InputThread(OutputStream o,boolean stop,boolean die) {
+		public InputThread(OutputStream o) {
 			this.out = o;
-			this.ended = stop;
-			this.diedok = die;
 		}
 		
 		public void run()  {
@@ -98,8 +100,6 @@ public class ConnectionChat extends Connection{
 					outputmessage = sc.nextLine();
 					if(outputmessage.equals("quit")) {
 						this.out.write(SymmetricKeyManager.cipher(key,"bye".getBytes()));
-						ended = true;
-						diedok = true;
 						break;
 					}
 					else
@@ -113,6 +113,33 @@ public class ConnectionChat extends Connection{
 				e.printStackTrace();
 				System.out.println("Error while trying to encrypt");
 			}
+			finally {
+				ended= true;
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		Socket s;
+		
+		try {
+			ServerSocket server_sock = new ServerSocket(5555);
+			System.out.println("Wait for a connection...");
+			s = server_sock.accept();
+		}
+		catch(BindException e) {
+			s = new Socket("localhost", 5555);
+		}
+		
+		ConnectionChat chat = new ConnectionChat("localhost", 5555, s, NeedhamSchroeder.generateSessionKey(BigInteger.ONE, BigInteger.TEN));
+		chat.bind();
+		chat.run();
+		chat.close();
+		if (chat.finishedWell())
+			System.out.println("Done.");
+		else {
+			System.out.println(chat.getErrorMessage());
 		}
 	}
 	
